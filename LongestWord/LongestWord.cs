1f5dce1eac1051dfe6200c5e33f2ab2267c924a2
Denis <Kds04042002omgu@gmail.com>
2022-06-22 01:22:56 +0600

    Добавьте файлы проекта.

---
+++ LongestWord/LongestWord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    internal class LongestWord
+    {
+        public static int Counter(string word)
+        {
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.IsLetter(word[i]))
+                    count++;
+
+            }
+            return count;
+        }
+    }
+    
+}
---
+++ LongestWord/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int max = 0;
+        string longest = "";
+        Console.WriteLine("Вводите слова, завершая каждое нажатием Enter");
+        Console.WriteLine("Для выхода наберите 'exit'");
+
+        string input = Console.ReadLine();
+        LongestWord.LongestWord.Counter(input);
+        if (LongestWord.LongestWord.Counter(input) > max)
+        {
+            max = LongestWord.LongestWord.Counter(input);
+            longest = input;
+        }
+
+        while (!input.Equals("exit"))
+        {
+            input  = Console.ReadLine();
+            LongestWord.LongestWord.Counter(input);
+            if (LongestWord.LongestWord.Counter(input) > max && !input.Equals("exit"))
+            {
+                max = LongestWord.LongestWord.Counter(input);
+                longest = input;
+            }
+        } 
+            Console.WriteLine("Считывание завершено.");
+        Console.WriteLine("Самое длинное слово: " + longest + "(" + max + ")");
+    }
+}
